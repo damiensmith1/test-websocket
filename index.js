dcb0217d2471d5843ace5ef1e9a0371167a50359
Damien Smith <19ds6@queensu.ca>
2025-02-07 16:49:28 -0500

    port change

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const WebSocket = require('ws');
 
 // Create a WebSocket server on port 8080
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({ port: 80 });
 
 // When a new client connects
 wss.on('connection', (ws) => {
